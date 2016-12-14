14dbf0a31c46c457ea0344749a6c9ba43b54639e
Painchaud Alexandre <painchaud.alex@gmail.com>
2016-12-14 21:03:55 +0100

    CHAT IS UP \o/

--- app.js
+++ app.js
@@ -2,29 +2,35 @@
  * Created by alex on 13/12/16.
  */
 
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var bodyParser = require('body-parser')
-
 var Contact = require('./models/contact');
-
-var app = express();
+var Member = require('./models/member');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+app.set('view engine', 'ejs');
 var contacts = [];
+
+
 
 app.get('/', function (req, res) {
     res.sendFile('views/index.html', {root: __dirname });
 });
 app.get('/contact/list', function (req, res) {
+    res.header('Content-Type','application/json');
  res.send(contacts);
 });
 
 app.post('/contact/add', function (req, res) {
-    contacts.push(new Contact(req.body.lastname, req.body.firstname));
+    var contact  = new Contact(req.body.lastname, req.body.firstname);
+    contacts.push(contact);
 
     var homeUrl = 'http://' + req.headers.host + '/';
     res.writeHead(301,
@@ -34,7 +40,40 @@ app.post('/contact/add', function (req, res) {
 
 
 
+})
+// CHAT PART
+var members = [];
+app.get('/chat', function (req, res) {
+    res.render('chat', {title: "MY SHEETY PART"});
 });
-app.listen(8080, function () {
-    console.log('=============== SERVER ON ===============');
+
+io.on('connection', function(socket){
+    socket.on('member_connect', function(nickname){
+        var member = new Member(socket.id, nickname);
+        members.push(member);
+        io.emit('member_connect', members);
+    });
+
+    socket.on('member_send_message', function(nickname, message){
+        var data = { "nickname" : nickname, "message" : message};
+        io.emit('member_receive_message', data);
+    });
+
+    socket.on('disconnect', function(){
+        var id = null;
+        for (var k in members) {
+            if(socket.id === members[k].id) {
+                id = members[k].id;
+                members.splice(k,1);
+                break;
+            }
+        }
+
+        io.emit('member_disconnect', socket.id);
+    });
+});
+
+
+http.listen(8080, function(){
+    console.log('===== SERVER ON =====');
 });
---
+++ models/member.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alex on 13/12/16.
+ */
+var Member = function(id,nickname) {
+    this.nickname = nickname;
+    this.id = id;
+}
+
+
+module.exports = Member;
